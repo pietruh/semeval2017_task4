28bb2a9c3f5389cf46383b79f8313982efab3ee2
PietruszkaMichal_HS <pietruszka@holosurgical.com>
2018-09-17 23:21:04 +0200

    Added possibility to use data_generator and make augmentation online during training.

--- pretrained_glove_embeddings_tuto.py
+++ pretrained_glove_embeddings_tuto.py
@@ -25,7 +25,9 @@ import keras
 from models import get_RNN_model_w_layer
 from nlp_utilities import path_builder, index_word_vectors, get_data, split_data, prepare_embedding_matrix, \
     macro_averaged_recall_tf, \
-    macro_averaged_recall_tf_soft
+    macro_averaged_recall_tf_soft, gpu_configuration_initialization
+
+from config import config, model_config, DEBUG
 
 # Import callbacks that will be passed to the fit functions
 from keras.callbacks import ModelCheckpoint, TensorBoard, EarlyStopping, LearningRateScheduler
@@ -33,47 +35,22 @@ from keras.callbacks import ModelCheckpoint, TensorBoard, EarlyStopping, Learnin
 from logger_to_file import Logger
 from data_generator import SynonymDataGenerator
 
-NAME_OF_THE_TRAIN_SESSION = "1_test_session"  # name of the training session to which all objects will be saved
+NAME_OF_THE_TRAIN_SESSION = "5_testing_augmented_data_full_model"  # name of the training session to which all objects will be saved
 PATH_TO_THE_LEARNING_SESSION = "./learning_sessions/" + NAME_OF_THE_TRAIN_SESSION + "/"
 
-
-
-# region main
 # ----------------------------------------------------------------------------------------------------------------------
+# region main
 
 if __name__ == "__main__":
     # first, build index mapping words in the embeddings set
     # to their embedding vector
+    print("DEBUG = {} \nconfig = {}\nmodel_config = {}".format(DEBUG, config, model_config))
+
+    # TODO: add gpu_configuration_initialization()
+    gpu_configuration_initialization()
+    use_generator = 0
     path_builder(PATH_TO_THE_LEARNING_SESSION)
     sys.stdout = Logger(PATH_TO_THE_LEARNING_SESSION + "log_training")
-
-    model_config = {"classes": 3,
-                    "max_length": 50,
-                    "masking": True,
-                    "return_sequences": True,
-                    "consume_less": 'cpu',
-                    "dropout_U": 0.3,
-                    "dropout_rnn": 0.3,
-                    "dropout_final": 0.5,
-                    "loss_l2": 0.0001,
-                    "clipnorm": 5.,
-                    "lr": 0.001,
-                    }
-
-    config = {"GLOVE_DIR": 'glove.6B',
-              "MAX_SEQUENCE_LENGTH": 50,
-              "OUTPUT_CLASSES": 3,
-              "MAX_NUM_WORDS": 200000,
-              "MAX_INDEX_CNT": 1000,  # 010000,
-              "EMBEDDING_DIM": 100,
-              "VALIDATION_SPLIT": 0.2,
-              "SYNONIMIZE_FRACTION": 0.2,
-              "SYNONIMIZE_WORDS": 4,
-              "SYNONIMIZE_WORDS_FRACTION": 0.1,
-              "model_config": model_config,
-              "BATCH_SIZE": 128,
-              "EPOCHS": 3
-              }
 
     print('Indexing word vectors.')
     filename_to_read = os.path.join(config["GLOVE_DIR"], 'glove.6B.100d.txt')
@@ -88,17 +65,17 @@ if __name__ == "__main__":
     print("Inspect results")
 
     # finally, vectorize the text samples into a 2D integer tensor
-    data, labels, word_index, tokenizer = get_data(train_directory, config, tokenizer=None, mode="training")
+    data, labels, word_index, tokenizer, texts = get_data(train_directory, config, tokenizer=None, mode="training")
 
     # split the data into a training set and a validation set
     x_train, y_train, x_val, y_val = split_data(data, labels, **config)
 
-    test_data, test_labels, test_word_index, test_tokenizer = get_data(test_directory, config, tokenizer=tokenizer,
-                                                                       mode="test")
+    test_data, test_labels, test_word_index, test_tokenizer, test_texts = get_data(test_directory, config,
+                                                                                   tokenizer=tokenizer,
+                                                                                   mode="test")
 
     # prepare embedding matrix
     embedding_matrix, num_words = prepare_embedding_matrix(word_index, embeddings_index, **config)
-
 
     # load pre-trained word embeddings into an Embedding layer
     # note that we set trainable = False so as to keep the embeddings fixed
@@ -120,40 +97,48 @@ if __name__ == "__main__":
     ## Callbacks - setup your callbacks for training generator
     # Create model checkpointer to save best models during training
     model_checkpoint = ModelCheckpoint('weights_ckpt.hdf5',
-                                            monitor='val_macro_averaged_recall_tf',
-                                            save_best_only=True)
+                                       monitor='val_macro_averaged_recall_tf',
+                                       save_best_only=True)
     callbacks.append(model_checkpoint)
 
     # Callback for stopping the training if no progress is achieved
     early_stopper = EarlyStopping(monitor='val_macro_averaged_recall_tf', min_delta=0, patience=0, verbose=0,
                                   mode='auto', baseline=None)
-    callbacks.append(early_stopper)
+    #callbacks.append(early_stopper)
 
     # Changing learning rate
-    #lr_scheduler = LearningRateScheduler(schedule, verbose=0)
+    # lr_scheduler = LearningRateScheduler(schedule, verbose=0)
 
     synonym_train_generator = SynonymDataGenerator(batch_size=config["BATCH_SIZE"], train_data_x=data,
                                                    train_data_y=labels,
                                                    embeddings_index=embeddings_index, word_index=word_index,
                                                    config=config)
 
+    print("Created DataGenerator object")
+    if use_generator:
+        print("Fitting generator")
 
-    hitory_generator = model.fit_generator(generator=synonym_train_generator,
-                                           batch_size=config["BATCH_SIZE"],
-                                           epochs=config["EPOCHS"],
-                                           validation_data=(x_val, y_val),
-                                           callbacks=callbacks
-                                           )
+        hitory_generator = model.fit_generator(generator=synonym_train_generator,
+                                               epochs=config["EPOCHS"],
+                                               steps_per_epoch=config["BATCH_SIZE"],
+                                               validation_data=(x_val, y_val),
+                                               callbacks=callbacks
+                                               )
+        print("Fitting generator finished!")
+        print("Evaluating generator!")
+        loss_metrics_eval = model.evaluate(x=test_data, y=test_labels, batch_size=config["BATCH_SIZE"])
+        print("Generator Evaluated !")
 
+    print("Fitting normal way ...!")
     history = model.fit(x_train, y_train,
                         batch_size=config["BATCH_SIZE"],
                         epochs=config["EPOCHS"],
                         validation_data=(x_val, y_val),
                         callbacks=callbacks)
-
-    model.save("rnn_model_b128_ep3_1_maxindex.h5")
-
+    print("Fitting normal way Finished...!")
+    model.save(PATH_TO_THE_LEARNING_SESSION + "rnn_model.h5")
 
     loss_metrics_eval = model.evaluate(x=test_data, y=test_labels, batch_size=config["BATCH_SIZE"])
     print("Evaluated metrics = {} \n {}".format(loss_metrics_eval, model.metrics_names))
 
+# endregion main
